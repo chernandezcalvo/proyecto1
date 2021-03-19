d80013998d6ac90ee78fa1e7b7487d58be52013e
Usuario 2 <usuario2@entornos.com>
2021-03-19 17:59:30 +0100

    Cambio 2 en Principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo Java-Git");
 		System.out.println("Cambio 1 en Principal");
+		System.out.println("Cambio 2 en Principal");
 
 	}
 
